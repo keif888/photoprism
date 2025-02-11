8acb5a3c9c9b0953e1fba7f7515b7880dcb49c34
Keith Martin <keith.martin@outlook.com.au>
2025-02-11 20:31:50 +1000

    Address change to fs.ImageJpeg

--- internal/dbbuilder/generate.go
+++ internal/dbbuilder/generate.go
@@ -667,7 +667,7 @@ func main() {
 			FileHash:     rnd.GenerateUID(entity.FileUID),
 			FileSize:     rand.Int64N(1000000),
 			FileCodec:    "",
-			FileType:     string(fs.ImageJPEG),
+			FileType:     string(fs.ImageJpeg),
 			MediaType:    string(media.Image),
 			FileMime:     "image/jpg",
 			FilePrimary:  true,
