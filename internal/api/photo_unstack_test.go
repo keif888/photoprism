ab468999d3fa391047a5a026e12e46a816c2c85e
Keith Martin <keith.martin@outlook.com.au>
2025-09-09 21:23:38 +1000

    Tests: Enable unstack success test

--- internal/api/photo_unstack_test.go
+++ internal/api/photo_unstack_test.go
@@ -2,9 +2,14 @@ package api
 
 import (
 	"net/http"
+	"os"
+	"path/filepath"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+
+	"github.com/photoprism/photoprism/internal/photoprism/get"
+	"github.com/photoprism/photoprism/pkg/fs"
 )
 
 func TestPhotoUnstack(t *testing.T) {
@@ -19,11 +24,20 @@ func TestPhotoUnstack(t *testing.T) {
 
 	t.Run("unstack bridge3.jpg", func(t *testing.T) {
 		app, router, _ := NewApiTest()
+
+		// Create test files so unstack can function
+		c := get.Config()
+		os.RemoveAll(filepath.Join(c.OriginalsPath(), "1990"))
+		os.RemoveAll(filepath.Join(c.OriginalsPath(), "London"))
+		fs.Copy("testdata/london_160x160.jpg", filepath.Join(c.OriginalsPath(), "1990", "04", "bridge2.jpg"))
+		fs.Copy("testdata/london_160x160.jpg", filepath.Join(c.OriginalsPath(), "London", "bridge3.jpg"))
+
 		PhotoUnstack(router)
 		r := PerformRequest(app, "POST", "/api/v1/photos/ps6sg6be2lvl0yh7/files/fs6sg6bwhhbnlqdn/unstack")
-		// TODO: Have a real file in place for testing the success case. This file does not exist, so it cannot be unstacked.
-		assert.Equal(t, http.StatusNotFound, r.Code)
-		// t.Logf("RESP: %s", r.Body.String())
+		assert.Equal(t, http.StatusOK, r.Code)
+		// Cleanup
+		os.RemoveAll(filepath.Join(c.OriginalsPath(), "1990"))
+		os.RemoveAll(filepath.Join(c.OriginalsPath(), "London"))
 	})
 
 	t.Run("not existing file", func(t *testing.T) {
